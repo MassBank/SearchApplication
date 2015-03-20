bc1ba85e69ae6459152017e69ec882cbced6de16
gayan <gayan@axiohelix.com>
2015-03-20 16:22:39 +0900

    introduce some error codes

--- app/exception/internal_exception.php
+++ app/exception/internal_exception.php
@@ -5,8 +5,10 @@ class Internal_Exception extends Exception
 	
 	private $http_status_code;
 	
-	public function __construct($code) {
-		$message = $this->code_to_message($code);
+	public function __construct($code, $message = NULL) {
+		if ( empty($message) ) {
+			$message = $this->code_to_message($code);
+		}
 		parent::__construct($message, $code);
 	}
 	
@@ -19,14 +21,25 @@ class Internal_Exception extends Exception
 		switch ($code) 
 		{
 			case Code::PARAM_ERROR_NO_PEAK_DATA:
-				$message = "No peak input data.";
+				$message = "No 'peak' input data.";
 				break;
-			case Code::PARAM_ERROR_ILLEGAL_CUTOFF:
-				$message = "Illegal value of cutoff threshold.";
+			case Code::PARAM_ERROR_NO_FORMULA:
+				$message = "No 'formula' input data.";
 				break;
-			case Code::PARAM_ERROR_ILLEGAL_PEAK:
-				$message = "Illegal value of peak.";
+				
+			case Code::PARAM_ERROR_INVALID_CUTOFF:
+				$message = "Invalid value of cutoff threshold.";
 				break;
+			case Code::PARAM_ERROR_INVALID_PEAK:
+				$message = "Invalid value of peak.";
+				break;
+			case Code::PARAM_ERROR_INVALID_VALUE_MODE:
+				$message = "Invalid value for parameter 'mode'.";
+				break;
+			case Code::PARAM_ERROR_INVALID_SEARCH_TYPE:
+				$message = "Invalid value for parameter 'search_type'.";
+				break;
+				
 			default:
 				$message = "Unknown error";
 				break;
